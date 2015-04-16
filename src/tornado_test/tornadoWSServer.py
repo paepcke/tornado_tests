2226859251168b83e5eeec11d439a750c3b5d118
Andreas Paepcke <paepcke@cs.stanford.edu>
2015-04-16 16:16:36 -0700

    Added class method getCertAndKey() to encapsulate finding of cert and key files.

--- src/tornado_test/tornadoWSServer.py
+++ src/tornado_test/tornadoWSServer.py
@@ -38,7 +38,30 @@ class CourseCSVServer(tornado.websocket.WebSocketHandler):
         print("WebSocket closed")
 
     def exportClass(self):
-        print("Export class was called.")   
+        print("Export class was called.")
+        
+    @classmethod
+    def getCertAndKey(cls):
+        '''
+        Return a 
+        
+        '''
+        homeDir = os.path.expanduser("~")
+        sslDir = '%s/.ssl/' % homeDir
+        try:
+            certFileName = next(fileName for fileName in os.listdir(sslDir) 
+	                               if fileName.endswith('.cer') or fileName.endswith('.crt'))
+        except StopIteration:
+            raise(ValueError("Could not find ssl certificate file in %s" % sslDir))
+        
+        try:
+            privateKeyFileName = next(fileName for fileName in os.listdir(sslDir) 
+	                                     if fileName.endswith('.key'))
+        except StopIteration:
+            raise(ValueError("Could not find ssl private key file in %s" % sslDir))
+        return (os.path.join(sslDir, certFileName),
+                os.path.join(sslDir, privateKeyFileName))
+           
 
 if __name__ == "__main__":
     
@@ -57,22 +80,11 @@ if __name__ == "__main__":
                                            ],
                                           debug=True)
 
-    homeDir = os.path.expanduser("~")
-    thisFQDN = socket.getfqdn()
-    thisFQDNUnderscored = thisFQDN.replace('.', '_')
-
-    sslRoot = '%s/.ssl/%s' % (homeDir, thisFQDN)
-    sslRootUnderscored = '%s/.ssl/%s' % (homeDir, thisFQDNUnderscored)
-    #*********
-    # For self signed certificate:
-    #sslRoot = '/home/paepcke/.ssl/server'
-    #*********
-
-    sslArgsDict = {
-     #"certfile": sslRoot + '.crt',
-     "certfile": sslRootUnderscored + '_cert.cer',
-     "keyfile":  sslRoot + '.key',
-     }
+    (certFile,keyFile) = CourseCSVServer.getCertAndKey()
+    sslArgsDict = {'certfile' : certFile,
+                   'keyfile'  : keyFile}
+                   
+    
 
     http_server = tornado.httpserver.HTTPServer(application,ssl_options=sslArgsDict)
 
